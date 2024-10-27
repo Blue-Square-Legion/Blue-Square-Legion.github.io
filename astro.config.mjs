64bfb7d5d4b52b25ce2841ff84ffbece9235b99a
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2024-10-26 19:39:50 -0700

    Add Flowbite UI Library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import svelte from "@astrojs/svelte";
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Blue Square Legion',
+    social: {
+      github: 'https://github.com/Blue-Square-Legion'
+    },
+    sidebar: [{
+      label: 'About Us',
+      autogenerate: {
+        directory: 'about'
+      }
+    }, {
+      label: 'Projects',
+      autogenerate: {
+        directory: 'project'
+      }
+      // items: [
+      // 	// Each item here is one entry in the navigation menu.
+      // 	{ label: 'Keito', link: '/project/keito/' },
+      // 	{ label: 'Eco Defender', link: '/project/eco-defender/' },
+      // ],
+    },
+    {
+      label: 'Guides',
+      autogenerate: {
+        directory: 'guides'
+      },
+    },
+    {
+      label: 'Tools',
+      autogenerate: {
+        directory: 'tools'
+      }
+    }]
+  }), svelte(), tailwind()]
 });
