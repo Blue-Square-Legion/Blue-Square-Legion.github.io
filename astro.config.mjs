fe6041c6540af364a19d4787125deccebeb11c61
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2024-10-28 20:08:21 -0700

    Update Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,37 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import svelte from "@astrojs/svelte";
-
 import tailwind from '@astrojs/tailwind';
+import { resolve } from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "Llue-Square-Legion.github.io",
+  // Commented out base due to note at: https://docs.astro.build/en/guides/deploy/github/#base
+  // base: "Blue-Square-Legion.github.io",
   integrations: [starlight({
     title: 'Blue Square Legion',
     social: {
       github: 'https://github.com/Blue-Square-Legion'
     },
     sidebar: [{
-      label: 'About Us',
-      autogenerate: {
-        directory: 'about'
-      }
-    }, {
-      label: 'Projects',
-      autogenerate: {
-        directory: 'project'
-      }
-      // items: [
-      // 	// Each item here is one entry in the navigation menu.
-      // 	{ label: 'Keito', link: '/project/keito/' },
-      // 	{ label: 'Eco Defender', link: '/project/eco-defender/' },
-      // ],
-    },
-    {
+      //   label: 'About Us',
+      //   autogenerate: {
+      //     directory: 'about'
+      //   }
+      // },
+      // {
+      //   label: 'Projects',
+      //   autogenerate: {
+      //     directory: 'project'
+      //   }
+      //   // items: [
+      //   // 	// Each item here is one entry in the navigation menu.
+      //   // 	{ label: 'Keito', link: '/project/keito/' },
+      //   // 	{ label: 'Eco Defender', link: '/project/eco-defender/' },
+      //   // ],
+      // },
+      // {
       label: 'Guides',
       autogenerate: {
         directory: 'guides'
@@ -40,5 +43,12 @@ export default defineConfig({
         directory: 'tools'
       }
     }]
-  }), svelte(), tailwind()]
+  }), svelte(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': resolve('./src/components'),
+      },
+    },
+  },
 });
