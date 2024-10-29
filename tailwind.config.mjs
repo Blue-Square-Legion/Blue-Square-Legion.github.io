274b7cc6777f5123b2285e2a6d6d97878c70b403
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2024-10-28 20:08:28 -0700

    Update Tailwind Config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,17 +8,30 @@ export default {
 		extend: {},
 	},
 	plugins: [
-		require('flowbite/plugin')
+		require('flowbite/plugin'),
 	],
 
-	darkMode: 'class',
-
+	darkMode: ['selector', '[data-theme="dark"]'],
 	theme: {
 		extend: {
 			colors: {
 				// flowbite-svelte
-				primary: { 50: '#FFF5F2', 100: '#FFF1EE', 200: '#FFE4DE', 300: '#FFD5CC', 400: '#FFBCAD', 500: '#FE795D', 600: '#EF562F', 700: '#EB4F27', 800: '#CC4522', 900: '#A5371B' },
-			}
+				primary: {
+					'50': '#f2f6fc',
+					'100': '#e1ebf8',
+					'200': '#cbdcf2',
+					'300': '#a7c5e9',
+					'400': '#7ca7de',
+					'500': '#5e89d3',
+					'600': '#4a70c6',
+					'700': '#3e5bb1',
+					'800': '#394d94',
+					'900': '#324376',
+					'950': '#222b49',
+				},
+
+			},
+
 		}
 	}
 }
