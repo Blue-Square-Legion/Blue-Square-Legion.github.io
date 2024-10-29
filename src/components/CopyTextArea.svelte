6bd032a8841e967029eeb152f3b7d6a8443f9152
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2024-10-28 20:21:11 -0700

    Add Seeking Member tool

---
+++ src/components/CopyTextArea.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Textarea, Button } from "flowbite-svelte";
+    let {
+        text = "",
+        rows = 30,
+        placeholder = "Nothing here yet",
+    }: { text: string; rows?: number; placeholder?: string } = $props();
+    let textareaprops = {
+        id: "copyable-area",
+        name: "copyable-area",
+        label: "Resulting Text",
+        rows,
+        placeholder,
+        readonly: true,
+    };
+
+    function copyText() {
+        if (navigator?.clipboard) {
+            navigator.clipboard.writeText(text);
+        }
+    }
+</script>
+
+<Button onclick="{copyText}" class=" bg-green-400"
+    >Copy Below Text To Clipboard</Button
+>
+<Textarea bind:value="{text}" {...textareaprops} class="block w-full text-lg" />
