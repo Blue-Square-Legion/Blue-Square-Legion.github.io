59cd8f3cf57e2494d2a82d805b8de8b73af37fc1
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2024-12-07 21:25:13 -0800

    Create Day View component
    Add day viewer for calender

---
+++ src/components/CycleCalender/DayView.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { Span } from "flowbite-svelte";
+
+    let {
+        id,
+        date,
+        events,
+        isCurrent = true,
+    }: {
+        id: string;
+        date: Date;
+        events: string;
+        isCurrent: boolean;
+    } = $props();
+
+    const userLocale =
+        navigator.languages && navigator.languages.length
+            ? navigator.languages[0]
+            : navigator.language;
+</script>
+
+<div
+    {id}
+    class=" border-primary-600 border-2 min-h-32 hover:text-lg rounded-xl p-1"
+>
+    {#if !isCurrent}
+        <div class="p-1">Outside Current Cycle</div>
+    {:else}
+        <Span class="text-md px-1"
+            >{date.toLocaleDateString(userLocale, {
+                day: "2-digit",
+                month: "2-digit",
+                year: "2-digit",
+            })}</Span
+        >
+        <br />
+        <Span class="text-sm">{events}</Span>
+    {/if}
+</div>
